24b024af3d6162cbbf4592925363cafcbbe92ed9
Caden Lee <maltyg3332@gmail.com>
2026-02-28 10:38:17 -0800

    blink test

--- hand-music/python/main.py
+++ hand-music/python/main.py
@@ -1,35 +1,42 @@
 from arduino.app_utils import *
-from arduino.app_bricks.web_ui import WebUI
+import time
 
 led_is_on = False
 
-def get_led_status():
-    """Get current LED status for API."""
-    return {
-        "led_is_on": led_is_on,
-        "status_text": "LED IS ON" if led_is_on else "LED IS OFF"
-    }
+#def get_led_status():
+#    """Get current LED status for API."""
+#    return {
+#        "led_is_on": led_is_on,
+#        "status_text": "LED IS ON" if led_is_on else "LED IS OFF"
+#    }
 
-def toggle_led_state(client, data):
-    """Toggle the LED state when receiving socket message."""
-    global led_is_on
-    led_is_on = not led_is_on
+#def toggle_led_state(client, data):
+#    """Toggle the LED state when receiving socket message."""
+#    global led_is_on
+#    led_is_on = not led_is_on
 
     # Call a function in the sketch, using the Bridge helper library, to control the state of the LED connected to the microcontroller.
     # This performs a RPC call and allows the Python code and the Sketch code to communicate.
-    Bridge.call("set_led_state", led_is_on)
+#    Bridge.call("set_led_state", led_is_on)
 
     # Send updated status to all connected clients
     #ui.send_message('led_status_update', get_led_status())
 
-def on_get_initial_state(client, data):
-    """Handle client request for initial LED state."""
-    #ui.send_message('led_status_update', get_led_status(), client)
-    pass
+#def on_get_initial_state(client, data):
+#    """Handle client request for initial LED state."""
+#    #ui.send_message('led_status_update', get_led_status(), client)
+#    pass
 
 # Handle socket messages (like in Code Scanner example)
 #ui.on_message('toggle_led', toggle_led_state)
 #ui.on_message('get_initial_state', on_get_initial_state)
 
+def loop():
+    global led_is_on
+    time.sleep(1)
+    led_is_on = not led_is_on
+    Bridge.call('set_led_state', led_is_on)
+    print("state toggled")
+
 # Start the application
-App.run()
+App.run(user_loop=loop)

d6592dd1fa37c2da59fdc56be8ebdf64200a7f0b
Dylan Liu <liudj5@uci.edu>
2026-02-28 13:02:52 -0800

    servo code idk

--- hand-music/sketch/sketch.ino
+++ hand-music/sketch/sketch.ino
@@ -1,21 +1,36 @@
 #include <Arduino_RouterBridge.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define NUM_SERVOS 16
+#define SERVOMIN  150   // Min pulse length (0 degrees)
+#define SERVOMAX  600   // Max pulse length (180 degrees)
+#define SERVO_FREQ 50   // 50 Hz for analog servos
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(9600);
+  Serial.println("HELLO!!");
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);
+  Bridge.provide("press_key", press_key);
+}
 
-    Bridge.begin();
-    Bridge.provide("set_led_state", set_led_state);
-    Bridge.provide("press_key", press_key);
+void setServoAngle(uint8_t channel, float angle) {
+  angle = constrain(angle, 0, 180);
+
+  uint16_t pwm_val = SERVOMIN + 
+                     (angle / 180.0) * (SERVOMAX - SERVOMIN);
+  pwm.setPWM(channel, 0, pwm_val);
+}
+
+void press_key(unint8_t key){
+    for(unit8_t key = 0; key < NUM_SERVOS; ++key){
+        setServoAngle(0, 0);
+    }
+    setServoAngle(0, 90);
 }
 
 void loop() {}
-
-void set_led_state(bool state) {
-    // LOW = on, HIGH = off
-    digitalWrite(LED_BUILTIN, state ? LOW : HIGH);
-}
-
-void press_key(double to_press) {
-    // TODO: do something
-}

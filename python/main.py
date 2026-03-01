5714659f596e0481b4576f18363e648733f6ec13
Dylan Liu <liudj5@uci.edu>
2026-03-01 03:00:33 -0800

    added ode to joy for dummy loop, I hope this works

---
+++ python/main.py
@@ -0,0 +1,162 @@
+from arduino.app_utils import *
+import time
+
+import cv2
+from PIL import Image
+import numpy as np
+from edge_impulse_linux.image import ImageImpulseRunner
+
+import argparse
+
+parser = argparse.ArgumentParser()
+# parser.add_argument("camera_no", default=0, type=int)
+parser.add_argument("-s", "--show", action="store_true")
+args = parser.parse_args()
+
+cap = cv2.VideoCapture(0)
+import urllib.request
+import os
+fname, _headers = urllib.request.urlretrieve("https://raw.githubusercontent.com/Big-Dyl/IrvineHacks2026/refs/heads/main/hand_landmark_detector_uno.eim") 
+os.chmod(fname, os.stat(fname).st_mode | 0o111)
+
+mdl = ImageImpulseRunner(fname)
+# mdl._allow_shm = False
+mdl.init()
+
+if not cap.isOpened():
+    print("ERROR: failed to open camera 0")
+
+led_is_on = False
+
+keyboard_state = {
+    'keys_down': [],
+    'active_key': -1
+}
+
+# For demo/visibility purposes
+def led_blink():
+    global led_is_on
+    led_is_on = not led_is_on
+    Bridge.call('set_led_state', led_is_on)
+
+def parse_landmarks(output_63: np.ndarray):
+    """
+    Convert flat [63] array of interleaved x,y,z values into
+    a list of 21 (px, py, z) tuples.
+    x and y are normalized [0,1] -> scaled to image pixels.
+    z is depth relative to wrist (not scaled).
+    """
+    coords = output_63.reshape(21, 3)  # shape (21, 3)
+    landmarks = []
+    for x, y, z in coords:
+        landmarks.append((x, y, float(z)))
+    return landmarks
+
+def call_model(frame):
+    global mdl
+
+    img = Image.fromarray(frame).resize((224, 224))
+    feats, crop = mdl.get_features_from_image(np.array(img))
+    got_back = mdl.classify(feats)
+    landmarks, hand_presence, handedness, world_landmarks = got_back["result"]["freeform"]
+
+    landmarks = parse_landmarks(np.array(landmarks))
+    # see airlib.LANDMARK_NAMES
+    return landmarks
+
+def find_keys_pressed(frame):
+    # Model returns x, y, z of each tip 0 to 192
+    locations = call_model(frame)
+    locations_zipped = {l: loc for l, loc in zip(locations, airlib.LANDMARK_NAMES)}
+    # Figure out whether finger is down based on the highest finger
+    pressed_finger_coordinates = []
+    for k, v in locations_zipped:
+        if k.endswith("_TIP"):
+            z_pos = v[2]
+            if z_pos > 10:
+                pressed_finger_coordinates.append(v)
+
+    # Imagine there are several white keys on screen; we partition them
+    # OR, determine the key width based on the average distance from the finger tips?
+    key_width = 192 / 8
+
+    # Depending on the pressed finger coordinates, press the corresponding keys
+    pressed = [False] * 8
+
+    for coord in pressed_finger_coordinates:
+        key_number = max(0, min(7, coord[0] // key_width))
+        pressed[key_number] = True
+
+    return pressed
+
+def activated_key(prev_frame, this_frame):
+    for i in range(0, 8):
+        if i < len(this_frame) and i < len(prev_frame) and this_frame[i] and not prev_frame[i]:
+            return i
+    return -1
+
+def press_key(to_press):
+    Bridge.call('press_key', to_press)
+
+def loop():
+    global keyboard_state
+    # global cap
+
+    time.sleep(0.50)
+    led_blink()
+
+    # Read from camera
+    if not cap:
+        # Cannot proceed
+        return
+    ret, frame = cap.read()
+    if not ret:
+        return
+
+    this_frame = find_keys_pressed(frame)
+    new_active_key = activated_key(keyboard_state['keys_down'], this_frame)
+    if new_active_key != keyboard_state['active_key']:
+        press_key(new_active_key)
+
+    keyboard_state['keys_down'] = this_frame
+    keyboard_state['active_key'] = new_active_key
+
+def dummy_loop():
+    press_key(3)
+    time.sleep(430)
+    press_key(3)
+    time.sleep(430)
+    press_key(4)
+    time.sleep(430)
+    press_key(5)
+    time.sleep(430)
+    press_key(5)
+    time.sleep(430)
+    press_key(4)
+    time.sleep(430)
+    press_key(3)
+    time.sleep(430)
+    press_key(2)
+    time.sleep(430)
+    press_key(1)
+    time.sleep(430)
+    press_key(1)
+    time.sleep(430)
+    press_key(2)
+    time.sleep(430)
+    press_key(3)
+    time.sleep(430)
+    press_key(3)
+    time.sleep(645)
+    press_key(1)
+    time.sleep(215)
+    press_key(1)
+    time.sleep(1)
+
+try:
+    # Start the application
+    App.run(user_loop=loop)
+finally:
+    cap.release()
+
+
---
+++ sketch/sketch.ino
@@ -0,0 +1,41 @@
+#include <Arduino_RouterBridge.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define NUM_SERVOS 16
+#define SERVOMIN  150   // Min pulse length (0 degrees)
+#define SERVOMAX  600   // Max pulse length (180 degrees)
+#define SERVO_FREQ 50   // 50 Hz for analog servos
+
+void setServoAngle(uint8_t channel, float angle) {
+  angle = constrain(angle, 0, 180);
+  uint16_t pwm_val = SERVOMIN + (angle / 180.0) * (SERVOMAX - SERVOMIN);
+  pwm.setPWM(channel, 0, pwm_val);
+}
+
+void set_led_state(bool state) {
+    digitalWrite(LED_BUILTIN, state ? LOW : HIGH);
+}
+
+void press_key(int key) {
+  for (uint8_t k = 0; k < NUM_SERVOS; ++k) {
+    setServoAngle(k, 0);
+  }
+
+  setServoAngle(key, key % 2 == 0 ? 27 : 90);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HELLO!!");
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);
+  Bridge.begin();
+  Bridge.provide("press_key", press_key);
+  Bridge.provide("set_led_state", set_led_state);
+}
+
+void loop() {}

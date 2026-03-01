617761c47131fec2b07cd6773efcc3f140220bcd
Dylan Liu <liudj5@uci.edu>
2026-02-28 20:50:35 -0800

    moved function

--- hand-music/sketch/sketch.ino
+++ hand-music/sketch/sketch.ino
@@ -9,6 +9,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMAX  600   // Max pulse length (180 degrees)
 #define SERVO_FREQ 50   // 50 Hz for analog servos
 
+void press_key(int key){
+    for(uint8_t k = 0; k < NUM_SERVOS; ++k){
+        setServoAngle(k, 0);
+    }
+    setServoAngle(key, 90);
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("HELLO!!");
@@ -24,13 +31,6 @@ void setServoAngle(uint8_t channel, float angle) {
   uint16_t pwm_val = SERVOMIN + 
                      (angle / 180.0) * (SERVOMAX - SERVOMIN);
   pwm.setPWM(channel, 0, pwm_val);
-}
-
-void press_key(int key){
-    for(uint8_t k = 0; k < NUM_SERVOS; ++k){
-        setServoAngle(k, 0);
-    }
-    setServoAngle(key, 90);
 }
 
 void loop() {}
